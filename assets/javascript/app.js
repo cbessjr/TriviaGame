f791f29bee9dfb40cac23370339c339fd3b8cd1b
Ender <cbessjr@gmail.com>
2018-11-03 18:08:21 -0700

    600pm 11/3/18 new commit to repair git repository error

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+// Array of objects to store the questions and answers
+//Each question is an object containing the correct answer
+//reference for the array: 
+var quesAnsw = ["red", "green"];
+    //{
+
+//     Question: "What color is the sky?",
+//     Selection: ["red", "green", "blue"],
+//     Answer: 2
+// },
+
+// {
+//     Question: "How many months in a year?",
+//     Selection: ["twelve", "fifteen", "ten"],
+//     Answer: 0
+// },
+
+// {
+//     Question: "Who makes the Playstation 4?",
+//     Selection: ["Microsoft", "Sony", "Nintendo"],
+//     Answer: 1
+//}];
+
+
+document.getElementById("test").innerHTML = quesAnsw[0];
+
+//Functions Functions Functions....
+//Start Function, use .html method
+
+// function Start {
+
+//     $("#start-game").html()
+
+
+// }
+//Use the form tag
+//Look up radio input
+
+
+
+//Function set the timer at 15 seconds
+//Worry about updating the div after the timer works
+//Timer for the form. Reference simpleRTimer (Timers, activity 8)
+
+
+
+//Function to evaluate the correct answers for the user
+//Google the evaluation of the checked radio buttons
+//.HTML to show the total correct and incorrect screen
+
+
+
+//Bonus, restart button
+
