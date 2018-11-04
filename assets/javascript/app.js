9d05b920c0446e28d2c660fda0f2cd22d0f5cb0c
Ender <cbessjr@gmail.com>
2018-11-03 21:18:24 -0700

    916pm 11/3/18. Final commit for the day. Played with the program for a few days. Attempted several solutions but coukd not get them to implement. I will ask for additional time to work with this problem.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 //Each question is an object containing the correct answer
 //reference for the array: 
 
-
+//attempted to create an array/object to hold the information for the questions. No success getting anything to display dymanically
 
 // var quesAnsw = ["red", "green"];
     //{
@@ -28,6 +28,8 @@
 // document.getElementById("test").innerHTML = quesAnsw[0];
 
 
+//_________________________________________________________________________________________________
+
 
 //Functions Functions Functions....
 //Start Function, use .html method
@@ -38,6 +40,8 @@
 
 
 // }
+
+
 //Use the form tag
 //Look up radio input
 
@@ -47,33 +51,107 @@
 //     //Female radio button is checked
 //   }
 
+
+//_________________________________________________________________________________________________
+
 //Function set the timer at 15 seconds
 //Worry about updating the div after the timer works
 //Timer for the form. Reference simpleRTimer (Timers, activity 8)
 
-for (var i = setTimeout(1000 *1); i < 31; i++) {
+//Additional timer references: 
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_countdown
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_settimeout2
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_settimeout_cleartimeout2
 
-setTimeout(oneSeconds, 1000 * 1);
-setTimeout(timeUp, 1000 * 30);
 
-function oneSeconds() {
-  // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-  // console log 10 seconds left
-  $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
-  console.log("10 seconds left");
+
+//attmepted to modidy the simple timer program to use here with no success
+
+// for (var i = setTimeout(1000 *1); i < 31; i++) {
+
+// setTimeout(oneSeconds, 1000 * 1);
+// setTimeout(timeUp, 1000 * 30);
+
+// function oneSeconds() {
+//   // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+//   // console log 10 seconds left
+//   $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
+//   console.log("10 seconds left");
+// }
+
+// function timeUp() {
+//   // in the element with an id of time-left add an h2 saying Time's Up!
+//   // console log done
+//   console.log("done");
+//   $("#time-left").append("<h2>Time's Up!</h2>");
+//   console.log("time is up");
+
+// }
+
+// // };
+
+
+//_________________________________________________________________________________________________
+
+// //Function to evaluate the correct answers for the user
+
+function questionCheck() {
+
+var totalAnsCorrect = 0;
+var totalAnsIncorrect = 0
+
+
+if (document.getElementById("ques-one-ans-three").checked) {
+
+    totalAnsCorrect = totalAnsCorrect + correctAns();
+
+} else {
+
+    totalAnsIncorrect = totalAnsIncorrect + incorrectAns();
 }
 
-function timeUp() {
-  // in the element with an id of time-left add an h2 saying Time's Up!
-  // console log done
-  console.log("done");
-  $("#time-left").append("<h2>Time's Up!</h2>");
-  console.log("time is up");
+if (document.getElementById("ques-two-ans-one").checked) {
+
+    totalAnsCorrect = totalAnsCorrect + correctAns();
+
+} else {
+
+    totalAnsIncorrect = totalAnsIncorrect + incorrectAns();
+}
+
+if (document.getElementById("ques-three-ans-two").checked) {
+
+    totalAnsCorrect = totalAnsCorrect + correctAns();
+
+} else {
+
+    totalAnsIncorrect = totalAnsIncorrect + incorrectAns();
+}
+
+    return totalAnsCorrect
+    return totalAnsIncorrect
 
 }
 
-};
-//Function to evaluate the correct answers for the user
+
+function correctAns() {
+
+    var correctAns = 0; 
+    correctAns++;
+
+    return correctAns;
+
+}
+
+function incorrectAns() {
+
+    var incorrectAns = 0;
+    incorrectAns++;
+
+    return incorrectAns;
+
+}
+
 //Google the evaluation of the checked radio buttons
 //.HTML to show the total correct and incorrect screen
 
